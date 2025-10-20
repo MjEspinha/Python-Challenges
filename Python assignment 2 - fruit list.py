beba51215f70ab789e0bf36ec367a31df1358f08
MjEspinha <157715297+MjEspinha@users.noreply.github.com>
2025-10-20 10:35:58 +0100

    Add files via upload

---
+++ Python assignment 2 - fruit list.py
@@ -0,0 +1,12 @@
+# Step 1: Create a list of 10 fruits
+fruits = ["apple", "orange", "banana", "kiwi", "grape", "pear", "mango", "pineapple", "cherry", "strawberry"]
+
+# Step 2: Alphabetize the list
+fruits.sort()
+
+# Step 3: Display the alphabetized list
+print("Alphabetized list of fruits:")
+print(fruits)
+print("Alphabetized list of fruits:")
+for fruit in fruits:
+    print(fruit)
